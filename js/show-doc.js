a79bfa974347b70f5cf548b1c1ea3c303d1b361d
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-13 13:54:28 -0400

    +

--- js/show-doc.js
+++ js/show-doc.js
@@ -6,21 +6,33 @@ $(document).ready(function () {
 function introduccion(){
     $(document).ready(function () {
         $('#contendido-pagina').load('./1.intro.html');
+        $("#options").css("display","none").slideUp(1000);
+        $("#cerrar").css("display","none");
+        $("#menu").css("display","inline");
     });
 }
 function informacion(){
     $(document).ready(function () {
         ruta = './2.informacion.html';
         $('#contendido-pagina').load('./2.informacion.html');
+        $("#options").css("display","none").slideUp(1000);
+        $("#cerrar").css("display","none");
+        $("#menu").css("display","inline");
     });
 }
 function obtener(){
     $(document).ready(function () {
         $('#contendido-pagina').load('./3.obtener-organizar.html');
+        $("#options").css("display","none").slideUp(1000);
+        $("#cerrar").css("display","none");
+        $("#menu").css("display","inline");
     });
 }
 function documentos(){
     $(document).ready(function () {
         $('#contendido-pagina').load('./documentos.html');
+        $("#options").css("display","none").slideUp(1000);
+        $("#cerrar").css("display","none");
+        $("#menu").css("display","inline");
     });
 }
