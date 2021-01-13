7406a86bbaa637ea5f060d2c6950d5a8559e7e93
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-13 13:30:08 -0400

    +

---
+++ js/show-doc.js
@@ -0,0 +1,26 @@
+var ruta = './1.intro.html';
+
+$(document).ready(function () {
+    $('#contendido-pagina').load(ruta);
+});
+function introduccion(){
+    $(document).ready(function () {
+        $('#contendido-pagina').load('./1.intro.html');
+    });
+}
+function informacion(){
+    $(document).ready(function () {
+        ruta = './2.informacion.html';
+        $('#contendido-pagina').load('./2.informacion.html');
+    });
+}
+function obtener(){
+    $(document).ready(function () {
+        $('#contendido-pagina').load('./3.obtener-organizar.html');
+    });
+}
+function documentos(){
+    $(document).ready(function () {
+        $('#contendido-pagina').load('./documentos.html');
+    });
+}
