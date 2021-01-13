9c1c8eb580fccbbf6dd1c05402f79222a5e9236f
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-13 12:23:29 -0400

    +

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+
+$("#menu").click(
+    function(){
+        $("#options").css("display","block").slideDown(1000);
+        $("#menu").css("display","none");
+        $("#cerrar").css("display","inline");
+    }
+    
+);
+$("#cerrar").click(
+    function(){
+        $("#options").css("display","none").slideUp(1000);
+        $("#cerrar").css("display","none");
+        $("#menu").css("display","inline");
+    }
+    
+);
