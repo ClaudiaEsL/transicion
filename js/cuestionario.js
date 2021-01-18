dacee1c9b53b70dec11eaecc5321635a1f7f3793
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-18 14:45:08 -0400

    +

---
+++ js/cuestionario.js
@@ -0,0 +1,5 @@
+$("#question-2").css("display","none");
+function next(){
+    $("#question-1").css("display","none");
+    $("#question-2").css("display","block").slideDown(slow);
+}
--- verificar.php
+++ verificar.php
@@ -1,11 +1,9 @@
 <?php
     echo "hi" ;
-
     $opcion1 =$_POST["opcion1"];
     if($opcion1 == true){
         echo("Respuesta correcta");
     }else{
         echo("Respuesta incorrecta");
     }
-
 ?>
