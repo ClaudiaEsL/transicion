ebaad8d594acdda2a66d8db1c99d92816943088b
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-19 14:48:47 -0400

    +

--- js/video.js
+++ js/video.js
@@ -1,7 +1,21 @@
-
 var mp4 = document.getElementById('video');
+var contenido = document.getElementById('texto');
+var btn = document.getElementById('volver');
+var btnaudio = document.getElementById('btn-audio');
 /*Ver tutorial*/
 function verVideo(){
+    btn.style = "display: block;"
     mp4.style = "display: block;"
+    contenido.style = "display:none;"
+    btnaudiobtn.style = "display: block;".style = "display:none;"
     mp4.play();
+}
+/*Cerrar video*/
+
+function volver(){
+    btn.style = "display: none;"
+    mp4.style = "display: none;"
+    contenido.style = "display:block;"
+    btnaudio.style = "display:block;"
+    mp4.pause();
 }
