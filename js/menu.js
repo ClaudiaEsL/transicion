bc36f7a042b9eb8486d1e5d9b92e14e0dd336033
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-19 15:16:25 -0400

    video

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-
+/*Abrir menu */
 $("#menu").click(
     function(){
         $("#options").css("display","block").slideDown(1000);
@@ -7,11 +7,11 @@ $("#menu").click(
     }
     
 );
+/*Cerrar menu */
 $("#cerrar").click(
     function(){
         $("#options").css("display","none").slideUp(1000);
         $("#cerrar").css("display","none");
         $("#menu").css("display","inline");
     }
-    
 );
--- js/video.js
+++ js/video.js
@@ -4,18 +4,18 @@ var btn = document.getElementById('volver');
 var btnaudio = document.getElementById('btn-audio');
 /*Ver tutorial*/
 function verVideo(){
-    btn.style = "display: block;"
-    mp4.style = "display: block;"
-    contenido.style = "display:none;"
-    btnaudiobtn.style = "display: block;".style = "display:none;"
+    btn.style = "display: block;";
+    mp4.style = "display: block;";
+    contenido.style = "display:none;";
+    btnaudio.style = "display: block;";
     mp4.play();
 }
 /*Cerrar video*/
 
 function volver(){
-    btn.style = "display: none;"
-    mp4.style = "display: none;"
-    contenido.style = "display:block;"
-    btnaudio.style = "display:block;"
+    btn.style = "display: none;";
+    mp4.style = "display: none;";
+    contenido.style = "display:block;";
+    btnaudio.style = "display:block;";
     mp4.pause();
 }
