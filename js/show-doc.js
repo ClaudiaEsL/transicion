85ff90e9a2ff07f7ed018d0ab337b7f3f275f63c
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-25 16:02:34 -0400

    arreglos

--- js/show-doc.js
+++ js/show-doc.js
@@ -1,23 +1,3 @@
-
-/*Prueba*/ 
-function introduccion(){
-    $(document).ready(function () {
-        $('#contendido-pagina').load('./1.intro.html');
-    });
-}
-function informacion(){
-    $(document).ready(function () {
-        ruta = './2.informacion.html';
-        $('#contendido-pagina').load('./2.informacion.html');
-    });
-}
-function obtener(){
-    $(document).ready(function () {
-        $('#contendido-pagina').load('./3.obtener-organizar.html');
-    });
-}
-function documentos(){
-    $(document).ready(function () {
-        $('#contendido-pagina').load('./documentos.html');
-    });
-}
+$(document).ready(function () {
+    $('#diapositivas').load('./diapositivas.html');
+});
--- js/video.js
+++ js/video.js
@@ -1,5 +1,4 @@
 var mp4 = document.getElementById('video');
-var contenido = document.getElementById('texto');
 var btn = document.getElementById('volver');
 var btnVerVideo = document.getElementById('btn-ver-video');
 var btnaudio = document.getElementById('btn-audio');
@@ -8,7 +7,6 @@ function verVideo(){
     btnVerVideo.style = "display:none;";
     btn.style = "display: block;";
     mp4.style = "display: block;";
-    contenido.style = "display:none;";
     btnaudio.style = "display: none;";
     mp4.play();
 }
@@ -18,7 +16,6 @@ function volver(){
     btnVerVideo.style = "display:block;";
     btn.style = "display: none;";
     mp4.style = "display: none;";
-    contenido.style = "display:block;";
     btnaudio.style = "display:block;";
     mp4.pause();
 }
