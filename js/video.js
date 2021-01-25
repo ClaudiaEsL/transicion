a60fa01f78534aae4b8932ad4ad22daf804a4a6b
ClaudiaEL <58369044+ClaudiaEsL@users.noreply.github.com>
2021-01-25 15:26:50 -0400

    commit

--- js/video.js
+++ js/video.js
@@ -1,9 +1,11 @@
 var mp4 = document.getElementById('video');
 var contenido = document.getElementById('texto');
 var btn = document.getElementById('volver');
+var btnVerVideo = document.getElementById('btn-ver-video');
 var btnaudio = document.getElementById('btn-audio');
 /*Ver tutorial*/
 function verVideo(){
+    btnVerVideo.style = "display:none;";
     btn.style = "display: block;";
     mp4.style = "display: block;";
     contenido.style = "display:none;";
@@ -13,6 +15,7 @@ function verVideo(){
 /*Cerrar video*/
 
 function volver(){
+    btnVerVideo.style = "display:block;";
     btn.style = "display: none;";
     mp4.style = "display: none;";
     contenido.style = "display:block;";
